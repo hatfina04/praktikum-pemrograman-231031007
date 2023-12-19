d746b6d2013aab47de696bac2ec9bc03324d9285
hatfina04 <153788330+hatfina04@users.noreply.github.com>
2023-12-19 18:01:33 +0800

    Add files via upload

---
+++ praktikum-a1.py
@@ -0,0 +1,92 @@
+print('Praktikum-1')
+# Print Nama Lengkap, NIM, Prodi
+print('Nama Lengkap : Hatfina Rustamin')
+print('NIM          : 231031007')
+print('Prodi        : Sistem Informasi')
+print()
+x=2023
+print('Angkatan',x)
+print()
+
+a=10
+print('Data adalah: ',a)
+print('tipe data adalah',type(a))
+print()
+
+b=10.0
+print('Data adalah: ',b)
+print('tipe data adalah',type(b))
+print()
+
+kampus = 'Institut Teknologi BJ Habibie'
+print('Data adalah: ',kampus)
+print('tipe data adalah',type(kampus))
+print()
+
+c=complex(2,3)
+print('Data adalah: ',c)
+print('tipe data adalah',type(c))
+print()  
+
+log = True
+print('Data adalah: ',log)
+print('tipe data adalah',type(log))
+print()
+
+p=20
+q=23
+hasil = p+q
+print('Hasil',p,'+',q,'=',hasil)
+print('Ini adalah batas untuk tugas praktikum###############')
+print()
+print()
+print()
+
+print('Penjumlahan')
+a=75
+b=15
+hasil = a+b
+print('Hasil', a, '+', b, '=', hasil)
+print()
+
+print('Pengurangan')
+c=85
+d=30
+hasil = c-d
+print('Hasil', c, '-', d, '=', hasil)
+print()
+
+print('Perkalian')
+e=11
+f=7
+hasil = e*f
+print('Hasil', e, '*', f, '=', hasil)
+print()
+
+print('Pembagian')
+g=81
+h=9
+hasil = g/h
+print('Hasil', g, '/', h, '=', hasil)
+print()
+
+print('Modulus')
+i=20
+j=3
+hasil = i%j
+print('Hasil', i, '%', j, '=', hasil)
+print()
+
+print('Eksponensial')
+k=12
+l=2
+hasil = k**l
+print('Hasil', k, '**', l, '=', hasil)
+print()
+
+print('Floor Division')
+m=100
+n=5
+hasil = m//n
+print('Hasil', m, '//', n, '=', hasil)
+
