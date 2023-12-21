d6e7ddc3334c2f5b2289e23126f7381af98a13a5
hatfina04 <153788330+hatfina04@users.noreply.github.com>
2023-12-21 19:26:58 +0800

    Latihan 3

---
+++ latihan 3.py
@@ -0,0 +1,7 @@
+nama = str(input('Masukkan Nama Karyawan: '))
+pendapatan = int(input('Pendapatan: '))
+
+if pendapatan > 1000:
+    print('Status: Berhak')
+else:
+    print('Status: Tidak Berhak')
