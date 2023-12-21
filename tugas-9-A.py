18e0b7f12e7be9271cfed5594a47cfae17059d36
hatfina04 <153788330+hatfina04@users.noreply.github.com>
2023-12-21 19:24:43 +0800

    Tugas-a9

---
+++ tugas-9-A.py
@@ -0,0 +1,37 @@
+
+Biodata = {'nama'   : 'Hatfina Rustamin',
+          'nim'     : '231031007',
+          'prodi'   : 'Sistem Informasi', 
+          'kelas'   : 'SI23-A',
+          'jurusan' : 'Sains',
+          'ttl'     : 'Parepare, 4 Januari 2005',
+          'alamat'  : 'Jl. Petta Unga',
+          'agama'   : 'Islam',
+          'unv'     : 'Institut Teknologi Bacharuddin Jusuf Habibie'}
+
+print(Biodata.get('nama'))
+print(Biodata['nim'])
+print()
+Biodata.update({'email' : 'hatfinafina04@gmail.com'})
+print(Biodata)
+print()
+print(Biodata.values())
+print()
+print(Biodata.get('email'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
