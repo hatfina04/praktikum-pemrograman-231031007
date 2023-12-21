cdf7ce504501b1504e653e6baa4d21f2fe85bec8
hatfina04 <153788330+hatfina04@users.noreply.github.com>
2023-12-21 19:27:42 +0800

    Latihan 6

---
+++ latihan 6.py
@@ -0,0 +1,29 @@
+#Fungsi Rekursif Fibonacci
+def fibonacci(n):
+    if n<0:
+        print('Tidak ada bilangan yang bernilai negatif')
+    if n==0 or n==1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+#Program Utama
+nilai=20
+hasil=fibonacci(nilai)
+print('Finobacci(%d)=%d' % (nilai,hasil))
+
+print('\n')
+
+# Fungsi Rekursif Fibonacci
+def fibonacci(n):
+    if n < 0:
+        print('Tidak ada bilangan yang bernilai negatif')
+        return None
+    elif n == 0 or n == 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+#Program Utama
+nilai = int(input("Masukkan sebuah bilangan: "))
+hasil = fibonacci(nilai)
+print('Fibonacci(%d) = %d' % (nilai, hasil))
